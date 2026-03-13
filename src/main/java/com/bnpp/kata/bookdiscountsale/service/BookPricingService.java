a7731aafe4a4ad9e30e2cb7c785c65324487edbd
Pradhi13 <pradheepthatsmyname@gmail.com>
2026-03-13 11:18:22 +0530

    Single Book price testcase added

---
+++ src/main/java/com/bnpp/kata/bookdiscountsale/service/BookPricingService.java
@@ -0,0 +1,9 @@
+package com.bnpp.kata.bookdiscountsale.service;
+
+public class BookPricingService {
+
+    public double calculateOrderDetails(String book) {
+        return 50.0;
+
+    }
+}
---
+++ src/test/java/com/bnpp/kata/bookdiscountsale/service/BookPricingServiceTests.java
@@ -0,0 +1,26 @@
+package com.bnpp.kata.bookdiscountsale.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BookPricingServiceTests {
+
+    private BookPricingService bookPricingService;
+
+    @BeforeEach
+    public void setup() {
+        bookPricingService = new BookPricingService();
+    }
+
+    @Test
+    @DisplayName("Single book price should be 50")
+    public void calculatePrice() {
+        double price = bookPricingService.calculateOrderDetails("Clean Code");
+        assertEquals(50.0, price);
+    }
+}
