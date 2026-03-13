96c3a2bf7f7de04214be4d3b502461f6f06e1dd2
Pradhi13 <pradheepthatsmyname@gmail.com>
2026-03-13 13:08:33 +0530

    Initial commit

---
+++ src/main/java/com/bnpp/kata/bookdiscountsale/BookdiscountsaleApplication.java
@@ -0,0 +1,13 @@
+package com.bnpp.kata.bookdiscountsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdiscountsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdiscountsaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnpp/kata/bookdiscountsale/BookdiscountsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpp.kata.bookdiscountsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdiscountsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
